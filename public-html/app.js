4d933dbbc5751a7974c5c253956dad5031fe65f1
Toshiaki <conamn1185@gmail.com>
2021-02-27 17:43:01 +0900

    this is the basic

--- public-html/app.js
+++ public-html/app.js
@@ -1,5 +1,58 @@
-Vue.createApp(
-    {
+Vue.createApp({
+    data: function () {
+        return {
+            todoTitle: 'aaa',
+            todoDescription: '',
+            todoCategories: [],
+            selectedCategory: '',
+            todos: [],
+            categories: [],
+            hideDoneTodo: false,
+            searchWord: '',
+            order: "desc",
+            categoryName: '',
+        }
+    },
+    // computed: {
+    //     canCreateTodo: function () {
+    //         return this.todoTitle !== ''
+    //     },
+    //     canCreateCategory: function () {
+    //         return this.categoryName !== '' && !this.existsCategory
+    //     },
+    //     existsCategory: function () {
+    //         const categoryName = this.categoryName
 
-    }
-).mount("#app")
+    //         return this.categories.indexOf(categoryName) !== -1
+    //     },
+    // },
+    // methods: {
+    //     createTodo: function () {
+    //         if (!this.canCreateTodo) {
+    //             return
+    //         }
+
+    //         this.todos.push({
+    //             id: 'todo-' + Date.now(),
+    //             title: this.todoTitle,
+    //             description: this.todoDescription,
+    //             categories: this.todoCategories,
+    //             dateTime: Date.now(),
+    //             done: false,
+    //         })
+
+    //         this.todoTitle = ''
+    //         this.todoDescription = ''
+    //         this.todoCategories = []
+    //     },
+    //     createCategory: function () {
+    //         if (!this.canCreateCategory) {
+    //             return
+    //         }
+
+    //         this.categories.push(this.categoryName)
+
+    //         this.categoryName = ''
+    //     },
+    // },
+}).mount('#app')
