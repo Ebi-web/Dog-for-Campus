8a1d234531c89f5364f9afbbb4e93337fc28d0d9
toshiaki <conamn1185@gmail.com>
2021-07-11 16:32:18 +0900

    added guide page

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ st.set_page_config(layout="wide")
 
 import select_page as page1
 import show_page as page2
+import userGuide as page3
 
 pages = {
     "対象サイトの選択/サイト内容の取得": page1.page,
     "取得結果一覧": page2.page,
+    "ヘルプ":page3.page,
 }
 
 if "scraping_result_dict" not in st.session_state:
@@ -22,4 +24,6 @@ selection = st.sidebar.radio("", list(pages.keys()))
 if selection == "対象サイトの選択/サイト内容の取得":
     pages[selection]()
 elif selection == "取得結果一覧":
+    pages[selection]()
+elif selection == "ヘルプ":
     pages[selection]()
