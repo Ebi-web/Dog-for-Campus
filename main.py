bdbc9488744adaf12d16fae3b6dd83263bff1ba2
toshiaki <conamn1185@gmail.com>
2021-07-11 11:38:27 +0900

    refactor

--- main.py
+++ main.py
@@ -1,31 +1,25 @@
+from requests.sessions import session
 import streamlit as st
+
 st.set_page_config(layout="wide")
 
 import select_page as page1
 import show_page as page2
-import scraping_page as page3
-
 
 pages = {
-    "対象サイトの選択": page1.page,
-    "新着情報一覧":  page2.page,
-    "更新の実行": page3.page,
+    "対象サイトの選択/サイト内容の取得": page1.page,
+    "取得結果一覧": page2.page,
 }
 
-
-
-if "news" not in st.session_state:
-    st.session_state["news"] = {}
+if "scraping_result_dict" not in st.session_state:
+    st.session_state["scraping_result_dict"] = {}
 if "sites" not in st.session_state:
     st.session_state["sites"] = {}
 
-st.sidebar.title('ページの一覧')
+st.sidebar.title("ページの一覧")
 selection = st.sidebar.radio("", list(pages.keys()))
 
-if selection == "対象サイトの選択":
-    is_scraping_list = pages[selection]()
-elif selection == "新着情報一覧":
-    pages[selection](st.session_state["news"])
-elif selection == "更新の実行" :
-    news_dict = pages[selection]()
-    st.session_state["news"] = news_dict
+if selection == "対象サイトの選択/サイト内容の取得":
+    pages[selection]()
+elif selection == "取得結果一覧":
+    pages[selection]()
